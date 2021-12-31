aaec3eabfa7827eba8ac1db28e10f0d6d110b72c
Chris <chrispauldillon@live.com>
2021-12-31 11:16:39 +0000

    Separated docker files by OS, added docker support for nethermind (.net client)

--- evm/scripts/routes.py
+++ evm/scripts/routes.py
@@ -1,5 +1,4 @@
 RESULT_CSV_OUTPUT_PATH = "/benchmark_results"
-# must be an absolute path to evm code dir
 EVM_CODE_DIR = "/input_data/evmcode"
 INPUT_VECTORS_DIR = "/input_data/input_vectors"
 PARITY_EVM_DIR = "/parity/target/release"
