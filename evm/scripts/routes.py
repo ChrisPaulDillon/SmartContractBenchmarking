fe6da5a0917dcb57326025fcfcf6840479fa9cbe
Chris <chrispauldillon@live.com>
2022-01-24 12:47:07 +0000

    Added kevm docker script, added ubuntu20.04 script for geth

--- evm/scripts/routes.py
+++ evm/scripts/routes.py
@@ -1,5 +1,8 @@
 RESULT_CSV_OUTPUT_PATH = "/benchmark_results"
 EVM_CODE_DIR = "/input_data/evmcode"
+KEVM_CODE_DIR = "/bin/legacytests/Constantinople/VMTests/vmArithmeticTest"
+
 INPUT_VECTORS_DIR = "/input_data/input_vectors"
 PARITY_EVM_DIR = "/parity/target/release"
 GETH_EVM_DIR = "/go-ethereum/core/vm/runtime"
+KEVM_EVM_DIR = "bin/kevm"
