6ee58479dc51fcfc9f46eb229b72e2c74fffbdea
Chris <chrispauldillon@live.com>
2022-01-25 11:59:56 +0000

    Parity & geth specific code now have their own service

--- evm/scripts/benchevm.py
+++ evm/scripts/benchevm.py
@@ -2,17 +2,12 @@
 
 from enum import Enum
 import json
-import re
-import subprocess
-import nanodurationpy as durationpy
+from services.parityservice import do_parity_bench, get_parity_cmd
+from services.gethservice import do_geth_bench, get_geth_cmd
 import csv
-import time
-import datetime
 import os
 import sys
-import shutil
-import shlex
-from routes import EVM_CODE_DIR, GETH_EVM_DIR, INPUT_VECTORS_DIR, PARITY_EVM_DIR, RESULT_CSV_OUTPUT_PATH
+from routes import EVM_CODE_DIR, INPUT_VECTORS_DIR, PARITY_EVM_DIR, RESULT_CSV_OUTPUT_PATH
 
 class EVMType(Enum):
      PARITY = "parity",
@@ -41,68 +36,6 @@ def save_results(evm_name, evm_benchmarks):
         for row in evm_benchmarks:
             writer.writerow(row)
 
-
-def get_parity_cmd(codefile, calldata, expected):
-    cmd_str = "./parity-evm --code-file {} --input {} --expected {} ".format(
-        codefile, calldata, expected)
-    return cmd_str
-
-
-def get_geth_cmd(codefile, calldata, expected):
-    cmd_str = "/go-ethereum/build/bin/evm --codefile {} --statdump --input {} --bench run".format(
-        codefile, calldata)
-    return cmd_str
-
-
-def do_parity_bench(parity_cmd):
-    #print("running parity-evm benchmark...\n{}\n".format(parity_cmd))
-    parity_cmd = shlex.split(parity_cmd)
-    stdoutlines = []
-    with subprocess.Popen(parity_cmd, cwd=PARITY_EVM_DIR, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1, universal_newlines=True) as p:
-        for line in p.stdout:  # b'\n'-separated lines
-            print(line, end='')
-            stdoutlines.append(line)  # pass bytes as is
-        p.wait()
-
-    timeregex = "code avg run time: ([\d\w\.]+)"
-    gasregex = "gas used: (\d+)"
-    # maybe --benchmark_format=json is better so dont have to parse "36.775k"
-    time_line = stdoutlines[-1]
-    gas_line = stdoutlines[-2]
-    time_match = re.search(timeregex, time_line)
-    time = durationpy.from_str(time_match.group(1))
-    gas_match = re.search(gasregex, gas_line)
-    gasused = gas_match.group(1)
-    return {'gas_used': gasused, 'time': time.total_seconds()}
-
-
-def do_geth_bench(geth_cmd):
-    #print("running geth-evm benchmark...\n{}\n".format(geth_cmd))
-    geth_cmd = shlex.split(geth_cmd)
-    stdoutlines = []
-    with subprocess.Popen(geth_cmd, cwd=GETH_EVM_DIR, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1) as p:
-        for line in p.stdout:  # b'\n'-separated lines
-            print(line.decode(), end='')
-            stdoutlines.append(line.decode())  # pass bytes as is
-        p.wait()
-    msOpRegex = "execution time:\s+([\d]+.[\d]+)ms"
-    qsOpRegex = "execution time:\s+([\d]+.[\d]+)µs"
-    gasregex = "EVM gas used:\s+(\d+)"
-    # maybe --benchmark_format=json is better so dont have to parse "36.775k"
-    time_line = stdoutlines[1]
-    gas_line = stdoutlines[0]
-    time_match = re.search(msOpRegex, time_line)
-    time = None
-    if time_match is None:
-        time_match = re.search(qsOpRegex, time_line)
-        time = durationpy.from_str("{}µs".format(time_match.group(1)))
-    else:
-        time = durationpy.from_str("{}ms".format(time_match.group(1)))
-    gas_match = re.search(gasregex, gas_line)
-    gasused = gas_match.group(1)
-    return {'gas_used': gasused, 'time': time.total_seconds()}
-
-
 def bench_evm(evm_name, input, codefilepath, shift_suffix):
     calldata = input['input']
     expected = input['expected']
@@ -112,7 +45,7 @@ def bench_evm(evm_name, input, codefilepath, shift_suffix):
 
     evm_type =  evm_name.split('-')[0]
 
-    if evm_type == EVMType.PARITY:
+    if evm_type == EVMType.PARITY.name.lower():
         parity_bench_cmd = get_parity_cmd(codefilepath, calldata, expected)
         parity_bench_result = do_parity_bench(parity_bench_cmd)
 
@@ -121,8 +54,8 @@ def bench_evm(evm_name, input, codefilepath, shift_suffix):
         evm_result['total_time'] = parity_bench_result['time']
         evm_result['gas_used'] = parity_bench_result['gas_used']
 
-    if evm_type == EVMType.GETH:
-        geth_bench_cmd = get_geth_cmd(codefilepath, calldata, expected)
+    if evm_type == EVMType.GETH.name.lower():
+        geth_bench_cmd = get_geth_cmd(codefilepath, calldata)
         geth_bench_result = do_geth_bench(geth_bench_cmd)
 
         evm_result['engine'] = "geth-evm"
---
+++ evm/scripts/services/parityservice.py
@@ -0,0 +1,32 @@
+
+import shlex
+import subprocess
+import re
+from routes import PARITY_EVM_DIR
+import nanodurationpy as durationpy
+
+def get_parity_cmd(codefile, calldata, expected):
+    cmd_str = "./parity-evm --code-file {} --input {} --expected {} ".format(
+        codefile, calldata, expected)
+    return cmd_str
+
+def do_parity_bench(parity_cmd):
+    #print("running parity-evm benchmark...\n{}\n".format(parity_cmd))
+    parity_cmd = shlex.split(parity_cmd)
+    stdoutlines = []
+    with subprocess.Popen(parity_cmd, cwd=PARITY_EVM_DIR, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1, universal_newlines=True) as p:
+        for line in p.stdout:  # b'\n'-separated lines
+            print(line, end='')
+            stdoutlines.append(line)  # pass bytes as is
+        p.wait()
+
+    timeregex = "code avg run time: ([\d\w\.]+)"
+    gasregex = "gas used: (\d+)"
+    # maybe --benchmark_format=json is better so dont have to parse "36.775k"
+    time_line = stdoutlines[-1]
+    gas_line = stdoutlines[-2]
+    time_match = re.search(timeregex, time_line)
+    time = durationpy.from_str(time_match.group(1))
+    gas_match = re.search(gasregex, gas_line)
+    gasused = gas_match.group(1)
+    return {'gas_used': gasused, 'time': time.total_seconds()}
